d9a8e30f418202776700022b53ee61495abf588d
ggaray-ik <ggaray0829@gmail.com>
2026-05-23 15:32:12 -0600

    GH project seems to be setup

---
+++ My project/Assets/Scripts/NoteView.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class NoteView : MonoBehaviour
+{
+    int lane;
+    float targetHitTime;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
